22044cb1b0dca55142d1f46319b6090255c36da6
Normal <thuonghuunguyen2002@gmail.com>
2022-11-10 10:37:11 +0700

    change js pos

--- index.php
+++ index.php
@@ -11,11 +11,9 @@
     <link rel="stylesheet" href="./asset/img/" />
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-    <script src="asset/js/js.js"></script>
+
     <?php session_start() ?>
 </head>
 <?php
@@ -320,6 +318,10 @@ include 'conn.php';
                 </div>
                 <!-- Copyright -->
             </footer>
+            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+            <script src="asset/js/js.js"></script>
             <!-- Footer -->
             <!-- Optional JavaScript -->
             <!-- jQuery first, then Popper.js, then Bootstrap JS -->
