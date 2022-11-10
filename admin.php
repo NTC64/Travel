bce54feb710bc2306b55e6c68c75ecd707912074
NTC <nguyenthanhchung.06042002@gmail.com>
2022-11-10 11:41:54 +0700

    add footer,header

--- admin.php
+++ admin.php
@@ -114,210 +114,29 @@
             </div>
         </nav>
 
-        <div class="p-4">
-            <div class="welcome">
-                <div class="content rounded-3 p-3">
-                    <h1 class="fs-3">Welcome to Dashboard</h1>
-                    <p class="mb-0">
-                        Hello Jone Doe, welcome to your awesome dashboard!
-                    </p>
-                </div>
+        <div class="admin__user">
+            <div class="container">
+                <form action="" method="get">
+                    <table>
+                        <tr>
+                            <td>Username</td>
+                            <td><input type="text" name="username" id=""></td>
+                        </tr>
+                        <tr>
+                            <td>Password</td>
+                            <td><input type="password" name="password" id=""></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td><input type="submit" value="Login" name="login"></td>
+                        </tr>
+                        
+                    </table>
+                </form>
             </div>
-
-            <section class="statistics mt-4">
-                <div class="row">
-                    <div class="col-lg-4">
-                        <div class="box d-flex rounded-2 align-items-center mb-4 mb-lg-0 p-3">
-                            <i class="uil-envelope-shield fs-2 text-center bg-primary rounded-circle"></i>
-                            <div class="ms-3">
-                                <div class="d-flex align-items-center">
-                                    <h3 class="mb-0">1,245</h3>
-                                    <span class="d-block ms-2">Emails</span>
-                                </div>
-                                <p class="fs-normal mb-0">Lorem ipsum dolor sit amet</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="box d-flex rounded-2 align-items-center mb-4 mb-lg-0 p-3">
-                            <i class="uil-file fs-2 text-center bg-danger rounded-circle"></i>
-                            <div class="ms-3">
-                                <div class="d-flex align-items-center">
-                                    <h3 class="mb-0">34</h3>
-                                    <span class="d-block ms-2">Projects</span>
-                                </div>
-                                <p class="fs-normal mb-0">Lorem ipsum dolor sit amet</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="box d-flex rounded-2 align-items-center p-3">
-                            <i class="uil-users-alt fs-2 text-center bg-success rounded-circle"></i>
-                            <div class="ms-3">
-                                <div class="d-flex align-items-center">
-                                    <h3 class="mb-0">5,245</h3>
-                                    <span class="d-block ms-2">Users</span>
-                                </div>
-                                <p class="fs-normal mb-0">Lorem ipsum dolor sit amet</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
-            <section class="charts mt-4">
-                <div class="row">
-                    <div class="col-lg-6">
-                        <div class="chart-container rounded-2 p-3">
-                            <h3 class="fs-6 mb-3">Chart title number one</h3>
-                            <canvas id="myChart"></canvas>
-                        </div>
-                    </div>
-                    <div class="col-lg-6">
-                        <div class="chart-container rounded-2 p-3">
-                            <h3 class="fs-6 mb-3">Chart title number two</h3>
-                            <canvas id="myChart2"></canvas>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
-            <section class="admins mt-4">
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="box">
-                            <!-- <h4>Admins:</h4> -->
-                            <div class="admin d-flex align-items-center rounded-2 p-3 mb-4">
-                                <div class="img">
-                                    <img class="img-fluid rounded-pill" width="75" height="75"
-                                        src="https://uniim1.shutterfly.com/ng/services/mediarender/THISLIFE/021036514417/media/23148906966/small/1501685402/enhance"
-                                        alt="admin" />
-                                </div>
-                                <div class="ms-3">
-                                    <h3 class="fs-5 mb-1">Joge Lucky</h3>
-                                    <p class="mb-0">
-                                        Lorem ipsum dolor sit amet consectetur elit.
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="admin d-flex align-items-center rounded-2 p-3 mb-4">
-                                <div class="img">
-                                    <img class="img-fluid rounded-pill" width="75" height="75"
-                                        src="https://uniim1.shutterfly.com/ng/services/mediarender/THISLIFE/021036514417/media/23148907137/small/1501685404/enhance"
-                                        alt="admin" />
-                                </div>
-                                <div class="ms-3">
-                                    <h3 class="fs-5 mb-1">Joge Lucky</h3>
-                                    <p class="mb-0">
-                                        Lorem ipsum dolor sit amet consectetur elit.
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="admin d-flex align-items-center rounded-2 p-3">
-                                <div class="img">
-                                    <img class="img-fluid rounded-pill" width="75" height="75"
-                                        src="https://uniim1.shutterfly.com/ng/services/mediarender/THISLIFE/021036514417/media/23148907019/small/1501685403/enhance"
-                                        alt="admin" />
-                                </div>
-                                <div class="ms-3">
-                                    <h3 class="fs-5 mb-1">Joge Lucky</h3>
-                                    <p class="mb-0">
-                                        Lorem ipsum dolor sit amet consectetur elit.
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="box">
-                            <!-- <h4>Moderators:</h4> -->
-                            <div class="admin d-flex align-items-center rounded-2 p-3 mb-4">
-                                <div class="img">
-                                    <img class="img-fluid rounded-pill" width="75" height="75"
-                                        src="https://uniim1.shutterfly.com/ng/services/mediarender/THISLIFE/021036514417/media/23148907114/small/1501685404/enhance"
-                                        alt="admin" />
-                                </div>
-                                <div class="ms-3">
-                                    <h3 class="fs-5 mb-1">Joge Lucky</h3>
-                                    <p class="mb-0">
-                                        Lorem ipsum dolor sit amet consectetur elit.
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="admin d-flex align-items-center rounded-2 p-3 mb-4">
-                                <div class="img">
-                                    <img class="img-fluid rounded-pill" width="75" height="75"
-                                        src="https://uniim1.shutterfly.com/ng/services/mediarender/THISLIFE/021036514417/media/23148907086/small/1501685404/enhance"
-                                        alt="admin" />
-                                </div>
-                                <div class="ms-3">
-                                    <h3 class="fs-5 mb-1">Joge Lucky</h3>
-                                    <p class="mb-0">
-                                        Lorem ipsum dolor sit amet consectetur elit.
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="admin d-flex align-items-center rounded-2 p-3">
-                                <div class="img">
-                                    <img class="img-fluid rounded-pill" width="75" height="75"
-                                        src="https://uniim1.shutterfly.com/ng/services/mediarender/THISLIFE/021036514417/media/23148907008/medium/1501685726/enhance"
-                                        alt="admin" />
-                                </div>
-                                <div class="ms-3">
-                                    <h3 class="fs-5 mb-1">Joge Lucky</h3>
-                                    <p class="mb-0">
-                                        Lorem ipsum dolor sit amet consectetur elit.
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
-            <section class="statis mt-4 text-center">
-                <div class="row">
-                    <div class="col-md-6 col-lg-3 mb-4 mb-lg-0">
-                        <div class="box bg-primary p-3">
-                            <i class="uil-eye"></i>
-                            <h3>5,154</h3>
-                            <p class="lead">Page views</p>
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-lg-3 mb-4 mb-lg-0">
-                        <div class="box bg-danger p-3">
-                            <i class="uil-user"></i>
-                            <h3>245</h3>
-                            <p class="lead">User registered</p>
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-lg-3 mb-4 mb-md-0">
-                        <div class="box bg-warning p-3">
-                            <i class="uil-shopping-cart"></i>
-                            <h3>5,154</h3>
-                            <p class="lead">Product sales</p>
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-lg-3">
-                        <div class="box bg-success p-3">
-                            <i class="uil-feedback"></i>
-                            <h3>5,154</h3>
-                            <p class="lead">Transactions</p>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
-            <section class="charts mt-4">
-                <div class="chart-container p-3">
-                    <h3 class="fs-6 mb-3">Chart title number three</h3>
-                    <div style="height: 300px">
-                        <canvas id="chart3" width="100%"></canvas>
-                    </div>
-                </div>
-            </section>
         </div>
     </section>
+
     <!-- partial -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.bundle.js"></script>
     <script
