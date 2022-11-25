b0427ca2f6dafd9a9b5c7ab5a6ff94cbc3636ff2
NTC <nguyenthanhchung.06042002@gmail.com>
2022-11-25 16:48:07 +0700

    bokk

---
+++ booking.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <link rel="stylesheet" href="./asset/css/style.css" />
+    <link rel="stylesheet" href="asset/font/fontawesome-free-6.1.2-web/css/all.min.css" />
+    <link rel="stylesheet" href="./asset/img/" />
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+
+    <?php session_start() ?>
+    <?php
+    include 'conn.php';
+    ?>
+</head>
+
+
+<body>
+    <!-- Login Form -->
+    <?php
+    include("header.php");
+    ?>
+    <!-- main -->
+    <div class="container hotTour my-4">
+        <div class="tour__title row mt-5">
+            <h2 class="col-12 text-uppercase p-0">Book Tour</h2>
+            <!-- <a class="m-auto text-right text-black-50 hvblack">View all >></a> -->
+        </div>
+        <div class="tour__title row mt-5">
+            <h3 class="col-10 text-uppercase p-0">Thông tin khách hàng</h3>
+            <form action="" method="post" class="fm">
+                <div class="row form my-3">
+
+                    <div class="col-6 left">
+
+                        <input type="text" placeholder="Họ và tên">
+                        <input type="text" placeholder="Số điện thoại">
+                        <input type="text" placeholder="Địa chỉ">
+                        <input type="text" placeholder="Tên tour">
+                        <input type="text" placeholder="Giá">
+                        <input type="text" placeholder="Số lượng người">
+                    </div>
+                    <div class="col-6 right">
+
+                        <input type="text" placeholder="Ngày khởi hành">
+                        <input type="text" placeholder="Ngày kết thúc">
+                        <textarea name="" id="" cols="30" rows="10" placeholder="Ghi chú"></textarea>
+                        <select name="" id="">
+                            <option value="">Chọn phương thức thanh toán</option>
+                            <option value="">Thanh toán trực tiếp</option>
+                            <option value="">Thanh toán ví điện tử</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row">
+                    <h3 class="col-12">Tổng tiền:</h3>
+                </div>
+                <div class="row btnbook">
+                    <input type="submit" class="btn btn-success" value="Đặt tour">
+                </div>
+
+            </form>
+
+        </div>
+        <!-- tour card -->
+
+        <!-- end tour card -->
+        <!-- tour card -->
+        <!-- ..... -->
+        <!-- end tour card -->
+        <!-- chi tiết tour -->
+        <div class="tourdetail">
+
+        </div>
+
+        <!---->
+
+    </div>
+    </div>
+    <!-- Footer -->
+    <?php include('footer.php') ?>
+    <!-- Footer -->
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+</body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<script src="asset/js/js.js"></script>
+
+</html>
